887ef62d61db674fd68519a6c00ae0dd65ae5ba7
Semiha Beyazkilic <semiha.beyazkilic@gmail.com>
2024-11-13 17:05:51 +0100

    Ajout de la fonction de descente de gradient et des fonctions de coût pour l'entraînement de la régression linéaire

--- ft_linear_regression/train.py
+++ ft_linear_regression/train.py
@@ -12,8 +12,28 @@ def load_csv(path: str) -> pd.DataFrame:
         print(f"Error: {e}")
 
 
-def model(X, theta):
+def model(X: np.ndarray, theta: np.ndarray):
     return X.dot(theta)
+
+
+def cost_function(X, y, theta):
+    m = len(y)
+    return 1/(2*m) * np.sum((model(X, theta) - y)**2)
+
+
+def grad(X, y, theta):
+    m = len(y)
+    # using ndarray's transpose `T()` function in the formula
+    return 1/m * X.T.dot(model(X, theta) - y)
+
+
+def gradient_descent(X, y, theta, learning_rate, n_iterations):
+
+    cost_history = np.zeros(n_iterations)
+    for i in range(0, n_iterations):
+        theta = theta - learning_rate * grad(X, y, theta)
+        cost_history[i] = cost_function(X, y, theta)
+    return theta, cost_history
 
 
 def train():
@@ -24,8 +44,23 @@ def train():
     x = x.reshape(x.shape[0], 1)
     y = y.reshape(y.shape[0], 1)
 
-    X = np.hstack(x, np.ones(x.shape))
-    theta = np.random.randn(2, 1)
+    # X matrix
+    X = np.hstack((x, np.ones(x.shape)))
+    theta = np.zeros((2, 1))
+    learning_rate = 0.01
+    n_iterations = 1000
+
+    theta_final, cost_history = gradient_descent(X, y, theta,
+                                                 learning_rate,
+                                                 n_iterations)
+
+    print(f"theta_final = {theta_final}")
+
+    predictions = model(X, theta_final)
+    plt.scatter(x, y)
+    plt.plot(x, predictions, c='r')
+    # plt.plot(range(1000), cost_history)
+    plt.show()
 
 
 def main():
